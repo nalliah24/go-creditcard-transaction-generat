ce6768684cba761c9b21f8297d0367bc6d050e3e
major24 <major.nalliah@gmail.com>
2022-03-07 19:19:20 -0500

    refactor main method

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -2,90 +2,111 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 
 	infra "github.com/nalliah24/go-creditcard-transaction-generator/pkg/bank/infracture"
+	m "github.com/nalliah24/go-creditcard-transaction-generator/pkg/bank/model"
 	ts "github.com/nalliah24/go-creditcard-transaction-generator/pkg/bank/service"
 )
 
-const (
-	isIndent = false
-)
-
 func main() {
-	genTransSequentially()
-	genTransConcurrently()
+	args := os.Args[1:]
+	genTransactions(args)
 }
 
-func genTransSequentially() {
-	fmt.Println("Generating mock transactions, based on 'config.json' file (Sequential)")
+func genTransactions(args []string) {
+	isIndent := false
+	outFileTrans := "output_trans"
+	outFileSummary := "output_summary"
+
+	if len(args) <= 0 {
+		fmt.Println("1st Argument required to process trans data: 1 = Sequential, 2 = Concurrent")
+		fmt.Println("2nd Argument optional: true = 'Indent' Json in output file")
+		os.Exit(1)
+	}
+
+	mode := args[0]
+	if mode != "1" && mode != "2" {
+		fmt.Println("1St argument must be 1 or 2")
+		os.Exit(1)
+	}
+
+	// check if json output as Indented?
+	if len(args) == 2 {
+		v, err := strconv.ParseBool(args[1])
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		isIndent = v
+	}
+
+	// read config
+	fmt.Println("Read configuratins from 'config.json'")
 	cfgs, err := infra.ReadConfig("config.json")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
+	// gen transactions
+	var trans m.TransactionList
 	start := time.Now()
-	fmt.Println("Calling transaction service to generate")
+
+	if args[0] == "1" {
+		fmt.Println("Generating mock transactions sequentially")
+		trans, err = genTransactionsSeq(cfgs)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		outFileTrans = fmt.Sprintf("%s_seq_%d.json", outFileTrans, len(trans))
+	}
+
+	if args[0] == "2" {
+		fmt.Println("Generating mock transactions concurrently")
+		trans, err = genTransactionsConc(cfgs)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		outFileTrans = fmt.Sprintf("%s_conc_%d.json", outFileTrans, len(trans))
+	}
+
+	elapsed := time.Since(start)
+	fmt.Println("Time took: ", elapsed)
+
+	fmt.Println("Calling file handler to write transactions: ", outFileTrans)
+	infra.WriteJson(outFileTrans, trans, isIndent)
+
+	// prepare summary
+	summaries, err := trans.PrepareSummary(cfgs)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	outFileSummary = fmt.Sprintf("%s_%d.json", outFileSummary, len(trans))
+
+	fmt.Println("Calling file handler to write summary: ", outFileSummary)
+	infra.WriteJson(outFileSummary, summaries, isIndent)
+
+	fmt.Println("Done")
+}
+
+func genTransactionsSeq(cfgs []m.Config) (m.TransactionList, error) {
 	trans, err := ts.GenerateAllTransactions(cfgs)
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		return nil, err
 	}
-	elapsed := time.Since(start)
-	fmt.Println(elapsed)
-
-	// total trans for file output name
-	numOfTrans := len(trans)
-
-	fmt.Println("Calling file handler to write 'output_n.json' file")
-	infra.WriteJson(fmt.Sprintf("output_s_%d.json", numOfTrans), trans, isIndent)
-
-	// prepare summary
-	summaries, err := trans.PrepareSummary(cfgs)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	fmt.Println("Calling file handler to write 'output_summary_n.json' file")
-	infra.WriteJson(fmt.Sprintf("output_s_summary_%d.json", numOfTrans), summaries, isIndent)
-
-	fmt.Println("Done")
+	return trans, nil
 }
 
-func genTransConcurrently() {
-	fmt.Println("Generating mock transactions, based on 'config.json' file (Concurrent)")
-	cfgs, err := infra.ReadConfig("config.json")
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	start := time.Now()
-	fmt.Println("Calling transaction service to generate")
+func genTransactionsConc(cfgs []m.Config) (m.TransactionList, error) {
 	trans, err := ts.GenerateAllTransactionsConc(cfgs)
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		return nil, err
 	}
-	elapsed := time.Since(start)
-	fmt.Println(elapsed)
-
-	// total trans for file output name
-	numOfTrans := len(trans)
-
-	fmt.Println("Calling file handler to write 'output_n.json' file")
-	infra.WriteJson(fmt.Sprintf("output_c_%d.json", numOfTrans), trans, isIndent)
-
-	// prepare summary
-	summaries, err := trans.PrepareSummary(cfgs)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	fmt.Println("Calling file handler to write 'output_summary_n.json' file")
-	infra.WriteJson(fmt.Sprintf("output_c_summary_%d.json", numOfTrans), summaries, isIndent)
-
-	fmt.Println("Done")
+	return trans, nil
 }
-
